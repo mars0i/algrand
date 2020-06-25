aea29eef86da8bb3302144d3df9b3b64438a881b
Marshall Abrams <marshall@logical.net>
2020-06-25 12:45:13 -0500

    Added first draft of convert-string.
    This function takes a possibly floating-point string representation of
    a number in a given base, and parses the string as a number in the
    given base, returning the number.  (Currently as a Ratio.)

--- src/clj/utils/convertbase.clj
+++ src/clj/utils/convertbase.clj
@@ -1,10 +1,13 @@
 ;;;; Functions to convert bases
 (ns utils.convertbase
-    (:require [clojure.math.numeric-tower :as m]))
+    (:require [clojure.math.numeric-tower :as math]
+              [clojure.string :as string]))
 
 (defn div-with-rem
-  [x div]
-  [(quot x div) (mod x div)])
+  "Returns a pair consisting of the integer quotient of dividend/divisor
+  followed by divident mod divisor."
+  [dividend divisor]
+  [(quot dividend divisor) (mod dividend divisor)])
 
 (defn num-to-char
   "Converts integers in [10,35] to corresponding characters.  Which
@@ -38,7 +41,7 @@
   [x base]
   (lazy-seq 
     (let [shifted-x (* x base)
-          int-part (m/floor shifted-x)
+          int-part (math/floor shifted-x)
           fract-part (- shifted-x int-part)]
       (cons (int int-part)
             (convert-fract-seq fract-part base)))))
@@ -58,16 +61,17 @@
 
 ;; Only positive numbers and bases <= 36.
 (defn convert-number
-  "Given a float or rational number x, returns a string representation of the 
-  number in the given base with the specified number of digits after the 
-  decimal point.  Uses lowercase alphabetic characters for bases greater than 
-  10 and less than 37.  If the fourth argument exists and is not nil or false,
-  internal operations use Clojure Ratio numbers for the fractional part, which 
-  are slower but avoid floating-point errors."
-  [x base digits & [ratl]]
-  (let [x' (if ratl (rationalize x) x)
-        int-part (bigint (m/floor x'))
+  "Given a float or rational number x, returns a string representation of
+  the number in the given base with the specified number of digits after
+  the decimal point.  Uses lowercase alphabetic characters for bases
+  greater than 10 and less than 37.  By default uses Clojure Ratio numbers
+  internally.  If the fourth argument exists and is not nil or false, uses
+  doubles instead."
+  [x base digits & [use-double]]
+  (let [x' (if use-double (double x) (rationalize x))
+        int-part (bigint (m/floor x')) ; coerce away ".0", might as well use BigInt rather than Integer
         fract-part (- x' int-part)]
+    (println x x' int-part fract-part)
     (apply str 
            (concat
              (map num-to-lowercase
@@ -76,3 +80,27 @@
              (map num-to-lowercase
                   (take digits
                         (convert-fract-seq fract-part base)))))))
+
+
+(defn parse-int
+  "Function wrapper for Java method Integer/parseInt."
+  [s]
+  (Integer/parseInt s))
+
+;; Note the reduce fns for int-part and fract-part have different forms 
+;; because ;; left of decimal point, the first digit is unmultiplied, but
+;; right of the point, even the first digit is in units of 1/base.
+(defn convert-string
+  [s base]
+  (let [[int-string fract-string] (string/split s #"\.")
+        int-xs   (map parse-int (string/split int-string #""))
+        fract-xs (map parse-int (string/split fract-string #""))
+        int-part   (reduce (fn [acc n] (+ n (* acc base)))
+                           0 int-xs)
+        fract-part (reduce (fn [acc n] (/ (+ n acc) base))
+                           0 (reverse fract-xs))]
+    (+ int-part fract-part)))
+        
+
+
+
