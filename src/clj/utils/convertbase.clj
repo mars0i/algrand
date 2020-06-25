8c8283707c5de6af5fc1f7f5e9450a4b44f9030d
mabrams <mabrams@uab.edu>
2020-06-25 16:52:44 -0500

    small changes to convert-number, added docstring

--- src/clj/utils/convertbase.clj
+++ src/clj/utils/convertbase.clj
@@ -65,13 +65,12 @@
   the number in the given base with the specified number of digits after
   the decimal point.  Uses lowercase alphabetic characters for bases
   greater than 10 and less than 37.  By default uses Clojure Ratio numbers
-  internally.  If the fourth argument exists and is not nil or false, uses
-  doubles instead."
+  internally.  If the fourth argument use-double exists and is truthy,
+  the function uses doubles internally instead."
   [x base digits & [use-double]]
   (let [x' (if use-double (double x) (rationalize x))
-        int-part (bigint (m/floor x')) ; coerce away ".0", might as well use BigInt rather than Integer
+        int-part (bigint (math/floor x')) ; coerce away ".0", might as well use BigInt rather than Integer
         fract-part (- x' int-part)]
-    (println x x' int-part fract-part)
     (apply str 
            (concat
              (map num-to-lowercase
@@ -91,16 +90,19 @@
 ;; because ;; left of decimal point, the first digit is unmultiplied, but
 ;; right of the point, even the first digit is in units of 1/base.
 (defn convert-string
-  [s base]
+  "Given a string representation s of a number in the given base (possibly 
+  with a fractional part after the decimal point), returns a Clojure
+  number represented.  This is a Ratio by default.  If the third argument, 
+  use-double, is provided and is truthy, a double is returned instead."
+  [s base & [use-double]]
   (let [[int-string fract-string] (string/split s #"\.")
         int-xs   (map parse-int (string/split int-string #""))
         fract-xs (map parse-int (string/split fract-string #""))
         int-part   (reduce (fn [acc n] (+ n (* acc base)))
                            0 int-xs)
         fract-part (reduce (fn [acc n] (/ (+ n acc) base))
-                           0 (reverse fract-xs))]
-    (+ int-part fract-part)))
-        
-
-
-
+                           0 (reverse fract-xs))
+        result (+ int-part fract-part)]
+    (if use-double
+      (double result)
+      result)))
