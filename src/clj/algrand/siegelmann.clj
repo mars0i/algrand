91b289718c6880b2e264302237be24004bf40bfd
Marshall Abrams <marshall@logical.net>
2020-06-28 12:22:32 -0500

    Initial commit for experiments with Siegelmann's book

---
+++ src/clj/algrand/siegelmann.clj
@@ -0,0 +1,4 @@
+;; Functions for experimenting with ideas in Hava Siegelmann's book
+;; *Neural Networks and Analog Computation: Beyond the Turing Limit*
+(ns algrand.siegelmann
+    (require [utils.convertbase :as base]))
