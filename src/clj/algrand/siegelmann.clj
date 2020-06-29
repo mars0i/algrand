a19344a965384841189af24b56a94580e860199a
Marshall Abrams <marshall@logical.net>
2020-06-29 12:02:01 -0500

    added def of xsi-tilde, rewrote lambda-tilde
    Old lambda-tilde used loop/recur, vs even earlier version that
    used tail-recursive (but not compiled as such) code.
    New version uses reduce.

--- src/clj/algrand/siegelmann.clj
+++ src/clj/algrand/siegelmann.clj
@@ -4,6 +4,10 @@
     (:require [clojure.math.numeric-tower :as math]
               [utils.convertbase :as base]))
 
+;; convenience abbreviations
+(def n2s base/number-to-string)
+(def s2n base/string-to-number)
+
 (defn sigma
   "Linear sigma function: Returns x unless it's outside of [0,1], in
   which case 0 or 1--whichever is closest--is returned."
@@ -12,31 +16,44 @@
 	(> x 1) 1
 	:else x))
 
-;; loop/recur version
 (defn lambda-tilde
   "lambda-tilde [equation (4.6), p. 64] is Siegelmann's continuous version of
   Lambda, a left-shift on base-9 fractional numbers that use only even digits."
   [q]
   (let [q9 (* q 9)]
-    (loop [j 8 acc 0]
-          (if (neg? j)
-            acc
-            (recur (dec j)
-                   (+ acc 
-                      (* (math/expt -1 j)
-                         (sigma (- q9 j)))))))))
+    (reduce 
+      (fn [sum j] (+ sum 
+                     (* (math/expt -1 j)
+                        (sigma (- (* q 9) j)))))
+      0 (range 9))))
+    
 
-;(defn xsi-tilde
-;  "xsi-tilde [equation (4.7), p. 65] is Siegelmann's continuous version of Xsi,
-;  a select-leftmost-digit function on base-9 fractional numbers that use only 
-;  even digits."
+
+(defn xsi-tilde
+  "xsi-tilde [equation (4.7), p. 65] is Siegelmann's continuous version of Xsi,
+  a select-leftmost-digit function on base-9 fractional numbers that use only 
+  even digits."
+  [q]
+  (let [q9 (* q 9)]
+    (* 2
+       (loop [j 3 acc 0]
+         (if (neg? j)
+           acc
+           (recur (dec j)
+                  (+ acc
+                     (sigma (- q9 (* 2 j) -1)))))))))
+
+
+;; loop/recur version
+;(defn old2-lambda-tilde
+;  "lambda-tilde [equation (4.6), p. 64] is Siegelmann's continuous version of
+;  Lambda, a left-shift on base-9 fractional numbers that use only even digits."
 ;  [q]
 ;  (let [q9 (* q 9)]
-;           (aux (lambda (j acc)
-;		  (if (>= j 0)
-;		    (aux (- j 1)
-;			 (+ acc
-;			    (sigma (- q9 (* 2 j) -1)))))
-;		    acc))))
-;    (* 2 (aux 3 0))))
-
+;    (loop [j 8 acc 0]
+;      (if (neg? j)
+;        acc
+;        (recur (dec j)
+;               (+ acc 
+;                  (* (math/expt -1 j)
+;                     (sigma (- q9 j)))))))))
