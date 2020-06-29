506a1eea35ab4812a1a1a0f49378a046fbc785e0
Marshall Abrams <marshall@logical.net>
2020-06-29 12:20:10 -0500

    rewrote xsi-tilde with reduce

--- src/clj/algrand/siegelmann.clj
+++ src/clj/algrand/siegelmann.clj
@@ -24,12 +24,23 @@
     (reduce 
       (fn [sum j] (+ sum 
                      (* (math/expt -1 j)
-                        (sigma (- (* q 9) j)))))
+                        (sigma (- q9 j)))))
       0 (range 9))))
-    
-
 
 (defn xsi-tilde
+  "xsi-tilde [equation (4.7), p. 65] is Siegelmann's continuous version of Xsi,
+  a select-leftmost-digit function on base-9 fractional numbers that use only 
+  even digits."
+  [q]
+  (let [q9 (* q 9)]
+    (* 2
+       (reduce (fn [sum j]
+                   (+ sum
+                      (sigma (- q9 (inc (* 2 j))))))
+               0 (range 4)))))
+
+;; loop/recur version
+(defn old-xsi-tilde
   "xsi-tilde [equation (4.7), p. 65] is Siegelmann's continuous version of Xsi,
   a select-leftmost-digit function on base-9 fractional numbers that use only 
   even digits."
@@ -41,19 +52,4 @@
            acc
            (recur (dec j)
                   (+ acc
-                     (sigma (- q9 (* 2 j) -1)))))))))
-
-
-;; loop/recur version
-;(defn old2-lambda-tilde
-;  "lambda-tilde [equation (4.6), p. 64] is Siegelmann's continuous version of
-;  Lambda, a left-shift on base-9 fractional numbers that use only even digits."
-;  [q]
-;  (let [q9 (* q 9)]
-;    (loop [j 8 acc 0]
-;      (if (neg? j)
-;        acc
-;        (recur (dec j)
-;               (+ acc 
-;                  (* (math/expt -1 j)
-;                     (sigma (- q9 j)))))))))
+                     (sigma (- q9 (inc (* 2 j)))))))))))
