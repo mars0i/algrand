64561f85b426d45a94d334e3db7f2ea58f4a74dc
Marshall Abrams <marshall@logical.net>
2020-06-30 12:22:43 -0500

    intermediate work commit

--- src/clj/algrand/siegelmann.clj
+++ src/clj/algrand/siegelmann.clj
@@ -2,9 +2,9 @@
 ;; *Neural Networks and Analog Computation: Beyond the Turing Limit*
 (ns algrand.siegelmann
     (:require [clojure.math.numeric-tower :as math]
-              [utils.convertbase :as base]
-              [uncomplicate.neanderthal.core :as nc]
-              [uncomplicate.neanderthal.native :as nn]))
+              [utils.convertbase :as base])
+    (:use [uncomplicate.neanderthal.core :only [dot]]
+          [uncomplicate.neanderthal.native :only [dv]]))
 
 ;; convenience abbreviations
 (def n2s base/number-to-string)
@@ -44,19 +44,40 @@
                       (sigma (- q9 (inc (* 2 j))))))
                0 (range 4)))))
 
-(def network (vec (repeat 17 0)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; defining the network
+;; with Neanderthal
 
-(def network (nn/dv (repeat 17 0)))
+;; Encoding of single circuit (s/b many) from example 4.1.1 on p.62:
+(def c-hat (base/string-to-number "0.860424440444240222426044444" 9))
+
+(def u (base/string-to-number "0.1" 2))
+
+;; In the statement of lemma 4.1.2 on p.63, HTS says you need a 16-node
+;; net, but in the model on p.65, there are nodes 0 through 16, i.e.
+;; 17 nodes.  I add the input node u at the end (though that will have
+;; to change in the second step).
+(def network (dv (conj (vec (repeat 17 0)) u)))
+
+;; In the end I want to replace the vectors below with one big matrix,
+;; and the constants s/b args to a generation function,
+;; but it's easier to start by coding it all piecemeal.
+
+;(defn x0thru8
+;  [u net]
+;  (let [v (dv [0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0]
+;    ;          0  1  2  3  4  5  6  7  8   9   10 11 12 13 14 15 16
+;  (
 
 (defn x9+
-  [u c-hat net]
+  [u net]
   (sigma (* 2 u)))
 
 (defn x10+
-  [u c-hat net]
-  (let [v (nn/dv [1 -1  1 -1  1 -1  1 -1  1 c-hat 0  0  0  0  0  0  0])]
-    ;             0  1  2  3  4  5  6  7  8   9   10 11 12 13 14 15 16
-    (sigma (nc/dot net v))))
+  [u net]
+  (let [v (dv [1 -1  1 -1  1 -1  1 -1  1 c-hat 0  0  0  0  0  0  0])]
+    ;          0  1  2  3  4  5  6  7  8   9   10 11 12 13 14 15 16
+    (sigma (dot net v))))
 
 
 
