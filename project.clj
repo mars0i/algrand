192c62e0c3faa21ecfda41d3bed40014ccd563d6
Marshall Abrams <marshall@logical.net>
2020-06-30 12:22:22 -0500

    added timbre dependencies to silent slf4j warning

--- project.clj
+++ project.clj
@@ -6,8 +6,12 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [incanter/incanter "1.9.3"]
-		 ;[org.slf4j/slf4j "1.7.30"]
 		 [uncomplicate/neanderthal "0.32.0"]
+                 ;; https://groups.google.com/forum/#!topic/uncomplicate/fbXYkT4pEkc
+                 ;; Dragan wrote: "SLFJ is needed by the Java dependencies, where it is common that a library uses logging internally (for better, or for worse). You can silence it by configuring any no-op logging,
+                 ;; such as by adding timbre to your dependencies:"
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.19"]
                 ]
   :plugins  [[lein-with-env-vars "0.2.0"]] ; needed for Neanderthal?
   ;:env-vars {:DYLD_LIBRARY_PATH "/opt/intel/mkl/lib:/opt/intel/lib"}
