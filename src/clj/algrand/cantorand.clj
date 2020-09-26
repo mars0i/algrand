a77fd9bb95d436bc69c61238afffff62d8ff3290
Marshall Abrams <marshall@logical.net>
2020-09-26 14:07:12 -0500

    Assigned names to posix-lcg's constants

--- src/clj/algrand/cantorand.clj
+++ src/clj/algrand/cantorand.clj
@@ -14,10 +14,13 @@
                    (fn [oldval] (mod (+ (* oldval multiplier) increment)
                                      modulus)))))))
 
+(def posix-modulus (ma/expt 2N 32N))
+(def posix-multiplier 25214903917N)
+(def posix-increment 11N)
+
 ;; From Wikipedia Linear Congruential Generator.  Posix parameters, 
 ;; Java.util.Random, but these in seed sizes and bits from this one.
 ;; Divide by 2^32.0 to get floats in [0,1).
 (def posix-lcg 
   "Given a seed, returns an integer LCG function using Posix parameters."
-  (lcg-maker (ma/expt 2N 32N) 25214903917N 11N))
-
+  (lcg-maker posix-modulus posix-multiplier posix-increment))
