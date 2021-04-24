62413783b982a9f56f8d5eecedda1a1c0ef63066
Marshall Abrams <marshall@logical.net>
2021-04-24 14:54:00 -0500

    removed *-shift-mat, zero-mat; added shift-matrix.

--- src/clj/utils/matrix.clj
+++ src/clj/utils/matrix.clj
@@ -1,11 +1,12 @@
 ;;;; Matrix utility functions not provided by clojure.core.matrix
-(ns algrand.utils.matrix
+(ns utils.matrix
     (:require [clojure.core.matrix :as mx]))
 
 ;(mx/set-current-implementation :persistent-vector)
-;(mx/set-current-implementation :ndarray)
+(mx/set-current-implementation :ndarray) ; better at preserving ints
 ;(mx/set-current-implementation :vectorz)
 
+;; Maybe this can be done more simply with views
 (defn set-diag
   "Sets a one of the diagonals of matrix m to newval.  If offset = 0,
   this is the main diagonal.  If offset = -1, it's the subdiagonal.
@@ -17,25 +18,10 @@
                        oldval))
                  m))
 
-(defn right-shift-mat
-  "Returns a size X size matrix with 1's in the subdiagonal and
-  zeros elsehwere.  Multiplying on the left of a vector returns
-  the vector shifted one step down/right."
-  [size]
-  (set-diag (mx/zero-matrix size) -1 1))
-
-(defn left-shift-mat
-  "Returns a size X size matrix with 1's in the superdiagonal and
-  zeros elsehwere.  Multiplying on the left of a vector returns
-  the vector shifted one step up/left."
-  [size]
-  (set-diag (mx/zero-matrix size) 1 1))
-
-;; Commented out because other ops will convert to floats anyway.
-;; It's a lost cause.
-;(defn zero-mat
-;  "Version of clojure.core.matrix/zero-matrix that returns a zero matrix
-;  with integers rather than floats."
-;  [size]
-;  (mx/emap int                     
-;        (mx/zero-matrix size size))) ; returns doubles
+(defn shift-matrix
+  "Returns a shift matrix, i.e. a square matrix with zero everywhere except
+  on one of the diagonals, which will be all ones.  If offset is 1, the 
+  superdiagonal is filled with 1's; if offset is -1, the subdiagonal is 
+  filled.  Other offsets shift the diagonal 1's as you'd expect."
+  [size offset]
+  (mx/shift (mx/identity-matrix size) 0 offset))
