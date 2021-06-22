84e95e3c519c8f3f61abef67ffaa5de327f0f689
Marshall Abrams <marshall@logical.net>
2021-06-22 11:19:53 -0500

    added comment

--- src/clj/algrand/finitefield.clj
+++ src/clj/algrand/finitefield.clj
@@ -34,7 +34,7 @@
 
 ;; Primitive polynomials over F2 from Niederreiter & Winterhof p. 37:
 (def nw37-F2prim2 [1 1 1])
-(def nw37-F2prim3 [1 1 0 1])
+(def nw37-F2prim3 [1 1 0 1])  ; also in Aspnes "Notes on Finite Fields" p. 5
 (def nw37-F2prim4 [1 1 0 0 1])
 (def nw37-F2prim5 [1 0 1 0 0 1])
 (def nw37-F2prim6 [1 1 0 0 0 0 1])
