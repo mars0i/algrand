e1532aa2538ef0961ad2e025fb713f0a24cf2872
Marshall Abrams <marshall@logical.net>
2021-06-23 12:07:16 -0500

    Added Alanen & Knuth's table of elements of F81, primitive polynomial

--- src/clj/algrand/finitefield_examples.clj
+++ src/clj/algrand/finitefield_examples.clj
@@ -17,14 +17,97 @@
 (def poly5c [3 2 4 3])
 
 ;; In F5, division example, Lidl & Niederreiter _Finite Fields_, pp. 20f:
-(def ff20-dividend  [3 4 0 0 1 2])
-(def ff20-divisor   [1 0 3])
-(def ff20-quotient  [1 2 2 4])
-(def ff20-remainder [2 2])
+(def ff20dividend  [3 4 0 0 1 2])
+(def ff20divisor   [1 0 3])
+(def ff20quotient  [1 2 2 4])
+(def ff20remainder [2 2])
 
 ;; Primitive polynomials over F2 from Niederreiter & Winterhof p. 37:
-(def nw37-F2prim2 [1 1 1])
-(def nw37-F2prim3 [1 1 0 1])  ; also in Aspnes "Notes on Finite Fields" p. 5
-(def nw37-F2prim4 [1 1 0 0 1])
-(def nw37-F2prim5 [1 0 1 0 0 1])
-(def nw37-F2prim6 [1 1 0 0 0 0 1])
+(def nw37F2prim2 [1 1 1])
+(def nw37F2prim3 [1 1 0 1])  ; also in Aspnes "Notes on Finite Fields" p. 5
+(def nw37F2prim4 [1 1 0 0 1])
+(def nw37F2prim5 [1 0 1 0 0 1])
+(def nw37F2prim6 [1 1 0 0 0 0 1])
+
+;; F3^4 = F81 from Alanenen & Knuth. See doc/finitefieldTestsNotes2.txt.
+(def akF3prim4 [2 0 0 3 4]) ; primitive polynomial (pp. 321, 316, cf. p. 310)
+(def akF3_4 [[1 0 0 0] ; elements of F3^4 in order of powers of x, i.e. [0 1]
+             [0 1 0 0] ; x^1
+             [0 0 1 0] ; x^2
+             [0 0 0 1] ; x^3
+             [1 0 0 2] ; x^4
+             [2 1 0 1] ; x^5
+             [1 2 1 2] ; ...
+             [2 1 2 2]
+             [2 2 1 0]
+             [0 2 2 1]
+             [1 0 2 1]
+             [1 1 0 1]
+             [1 1 1 2]
+             [2 1 1 2]
+             [2 2 1 2]
+             [2 2 2 2]
+             [2 2 2 0]
+             [0 2 2 2]
+             [2 0 2 0]
+             [0 2 0 2]
+             [2 0 2 1]
+             [1 2 0 1]
+             [1 1 2 2]
+             [2 1 1 0]
+             [0 2 1 1]
+             [1 0 2 0]
+             [0 1 0 2]
+             [2 0 1 1]
+             [1 2 0 0]
+             [0 1 2 0]
+             [0 0 1 2]
+             [2 0 0 2]
+             [2 2 0 1]
+             [1 2 2 2]
+             [2 1 2 0]
+             [0 2 1 2]
+             [2 0 2 2]
+             [2 2 0 0]
+             [0 2 2 0]
+             [0 0 2 2]
+             [2 0 0 0]
+             [0 2 0 0]
+             [0 0 2 0]
+             [0 0 0 2]
+             [2 0 0 1]
+             [1 2 0 2]
+             [2 1 2 1]
+             [1 2 1 1]
+             [1 1 2 0]
+             [0 1 1 2]
+             [2 0 1 2]
+             [2 2 0 2]
+             [2 2 2 1]
+             [1 2 2 1]
+             [1 1 2 1]
+             [1 1 1 1]
+             [1 1 1 0]
+             [0 1 1 1]
+             [1 0 1 0]
+             [0 1 0 1]
+             [1 0 1 2]
+             [2 1 0 2]
+             [2 2 1 1]
+             [1 2 2 0]
+             [0 1 2 2]
+             [2 0 1 0]
+             [0 2 0 1]
+             [1 0 2 2]
+             [2 1 0 0]
+             [0 2 1 0]
+             [0 0 2 1]
+             [1 0 0 1]
+             [1 1 0 2]
+             [2 1 1 1]
+             [1 2 1 0]
+             [0 1 2 1]
+             [1 0 1 1]
+             [1 1 0 0]
+             [0 1 1 0]
+             [0 0 1 1]]) ; x^79  (note x^{81-1} = [1])
