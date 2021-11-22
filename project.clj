d9c1007d5717b047dcd7acf2c0043c884af850c5
Marshall Abrams <marshall@logical.net>
2021-11-22 12:06:54 -0600

    Fixed typo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject algrand "0.1.0"
   :description "Code for thinking about algorithmic randomness"
-  :url "ttps://github.com/mars0i/algrand"
+  :url "https://github.com/mars0i/algrand"
   :license {:name "Gnu General Public License version 3.0"
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
